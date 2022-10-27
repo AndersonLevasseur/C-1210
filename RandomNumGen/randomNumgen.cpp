48687155f75bce4c54fb7163ee313badf7ad1ad8
Anderson Levasseur <anderson.levasseur@gmail.com>
2022-10-26 21:46:11 -0400

    Rearranged to allow for easier reuse. No longer runs everything in the console.

--- RandomNumGen/randomNumgen.cpp
+++ RandomNumGen/randomNumgen.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include <time.h>
+#include <vector>
 
 using namespace std;
+
+vector<int> randomNumGen(int numOfNumsGenerated, int min, int max) {
+
+	int randomNum;
+	vector<int> output;
+
+	srand(time(0));
+	
+	for (int i = 0; i < numOfNumsGenerated; i++) {
+		randomNum = rand() % ((max - min + 1) + min);
+		output.push_back(randomNum);
+	}
+
+	return output;
+}
 
 int main() {
 	int numOfTimes = 0;
@@ -10,17 +26,22 @@ int main() {
 	int n;
 	int min = 0;
 	int max = 0;
+	vector<int> randomNums;
 
 	srand(time(0));
+
 	cout << "please enter min\n";
 	cin >> min;
 	cout << "please enter max\n";
 	cin >> max;
 
-	for (int i = 0; i < numOfTimes; i++) {
-	n = rand() % ((max - min + 1) + min);
+	randomNums = randomNumGen(numOfTimes, min, max);
 
-	cout << n;
+	for (int i = 0; i < numOfTimes; i++) {
+	n = randomNums.at(randomNums.size() - 1);
+	randomNums.pop_back();
+
+	cout << n << " ";
 	}
 
 	return 0;
